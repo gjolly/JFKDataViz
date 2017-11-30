8a0196da5932a0870b09fe9ba409ff767d495cbb
ImLemni <florian.billon@insa-lyon.fr>
2017-11-30 21:12:59 +0100

    Zoom on timeline

--- brush.js
+++ brush.js
@@ -13,36 +13,51 @@ window.onload = () => {
     .domain([new Date(1944, 0, 0), new Date(1974, 0, 0) - 1])
     .rangeRound([0, width]);
 
+
+  var zoom = d3.zoom()
+    .translateExtent([
+      [0, 0],
+      [width, height]
+    ])
+    .extent([
+      [0, 0],
+      [width, height]
+    ])
+    .on("zoom", zoomed);
+
   var svg2 = d3.select("#svgB").append("svg")
     .attr("width", "90%")
     .attr("height", "10%")
     .append("g");
 
-  svg2.append("g")
+  svg2.call(zoom);
+  axisBottom1 = d3.axisBottom(x)
+    .tickSize(-height)
+    .tickFormat(function() {
+      return null;
+    });
+  axisBottom2 = d3.axisBottom(x)
+    .tickPadding(0);
+  g1 = svg2.append("g")
     .attr("class", "axis axis--grid")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x)
-      .ticks(d3.timeYear) // One row per x year
-      .tickSize(-height)
-      .tickFormat(function() {
-        return null;
-      }))
+    .attr("transform", "translate(0," + height + ")");
+  g1
+    .call(axisBottom1)
     .selectAll(".tick")
     .classed("tick--minor", function(d) {
       return d.getYear();
     })
 
-  svg2.append("g")
+  g2 = svg2.append("g")
     .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x)
-      .ticks(d3.timeYear)
-      .tickPadding(0))
+    .attr("transform", "translate(0," + height + ")");
+  g2
+    .call(axisBottom2)
     .attr("text-anchor", null)
     .selectAll("text")
     .attr("x", 1);
 
-  svg2.append("g")
+  brush = svg2.append("g")
     .attr("class", "brush")
     .call(d3.brushX()
       .extent([
@@ -58,16 +73,19 @@ window.onload = () => {
       d1 = d0.map(d3.timeYear.round);
 
     // If empty when rounded, use floor & ceil instead.
-    if (d1[0] >= d1[1]) {
-      d1[0] = d3.timeYear.floor(d0[0]);
-      d1[1] = d3.timeYear.offset(d1[0]);
-    }
-    d3.select(this).transition().call(d3.event.target.move, d1.map(x));
+    // if (d1[0] >= d1[1]) {
+    //   d1[0] = d3.timeYear.floor(d0[0]);
+    //   d1[1] = d3.timeYear.offset(d1[0]);
+    // }
+    // d3.select(this).transition().call(d3.event.target.move, d1.map(x));
     return d1[0], d1[1]
   }
 
-  // "MATCH (p1:People)-[doc:SENDTO]->(p2:People)\
-  // WHERE doc.year > d1 and doc.year < d2\
-  // RETURN p1,p2,doc\
-  // LIMIT 15",
+  function zoomed() {
+    var t = d3.event.transform;
+    x.domain(t.rescaleX(x).domain());
+    g2.call(axisBottom2);
+    g1.call(axisBottom1);
+  }
+
 }
