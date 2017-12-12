f07d04e0de380b47196374281d7ad5935fd066ce
ImLemni <florian.billon@insa-lyon.fr>
2017-12-12 11:12:27 +0100

    Change serveur ip

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
-//const url = 'http://dataviz.gauthierjolly.com:8080';
-const url = 'http://localhost:7474/db/data/transaction/commit';
+const url = 'https://dataviz.gauthierjolly.com:8080';
+//const url = 'http://localhost:7474/db/data/transaction/commit';
 let data = JSON.stringify({
   "statements": [{
     "statement": "MATCH (p1:People)-[doc:SENDTO]->(p2:People)\
@@ -19,7 +19,8 @@ function graphFetch(url, data) {
     body: data,
     headers: new Headers()
   }
-  fetchData.headers.append('Content-Type', 'application/json; charset=UTF-8');
+  //fetchData.headers.append('Content-Type', 'application/json; charset=UTF-8');
+  fetchData.headers.append('Content-Type', 'text/plain; charset=UTF-8');
   fetch(url, fetchData).then(r => r.json())
     .then(function(data) {
       if (!data["errors"].length) {
@@ -33,7 +34,8 @@ function graphFetch(url, data) {
 }
 
 function peopleGraph(peopleName) {
-  const url = 'http://localhost:7474/db/data/transaction/commit';
+  //const url = 'http://localhost:7474/db/data/transaction/commit';
+  const url = "https://dataviz.gauthierjolly.com:8080"
   let data = JSON.stringify({
     "statements": [{
       "statement": "MATCH (p1:People)-[doc:SENDTO]->(p2:People)\
@@ -49,7 +51,8 @@ function peopleGraph(peopleName) {
     body: data,
     headers: new Headers()
   }
-  fetchData.headers.append('Content-Type', 'application/json; charset=UTF-8');
+  //fetchData.headers.append('Content-Type', 'application/json; charset=UTF-8');
+  fetchData.headers.append('Content-Type', 'text/plain; charset=UTF-8');
   fetch(url, fetchData).then(r => r.json())
     .then(function(data) {
       if (!data["errors"].length) {
@@ -132,7 +135,8 @@ $('#peoplesearch')
         }
         return resp;
       },
-      url: 'http://localhost:7474/db/data/transaction/commit',
+      //url: 'http://localhost:7474/db/data/transaction/commit',
+      url : "https://dataviz.gauthierjolly.com:8080",
       method: 'POST',
       beforeSend: function(settings) {
         settings.data = JSON.stringify({
@@ -147,7 +151,8 @@ $('#peoplesearch')
         return settings;
       },
       beforeXHR: function(xhr) {
-        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        //xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.setRequestHeader('Content-Type', 'text/plain; charset=UTF-8');
       },
     },
     onSelect: function(result, response) {
@@ -191,7 +196,8 @@ $('#documentsearch')
         graphObject.restart()
         addElements(viz)
       },
-      url: 'http://localhost:7474/db/data/transaction/commit',
+      //url: 'http://localhost:7474/db/data/transaction/commit',
+      url : "https://dataviz.gauthierjolly.com:8080",
       method: 'POST',
       beforeSend: function(settings) {
         settings.data = JSON.stringify({
